32991fa998c3e3122fac227ee499bafbcd61cf57
mohammadbisamil <m.basim.ismail@gmail.com>
2022-09-03 18:44:34 +0300

    unfinished_task

--- script.js
+++ script.js
@@ -10,64 +10,64 @@
 //     document.querySelector("#equals"),
 //     document.querySelector("#display"),
 //   ],
-let calcElements = [
-  // document.querySelectorAll(".Num"),
-  // document.querySelectorAll(".operator"),
-  // document.querySelector(".dot"),
-  // document.querySelector(".clear"),
-  document.querySelector("#equals"),
-  document.querySelector("#display"),
-];
-let pressed = []; //array to save 1)1st pressed button 2)operator 3)2nd pressed button 4)equals
-let operator = []; //to know which operator is pressed
-let flag = false; // if operator is pressed this will become true
-function add(val1, val2) {
-  return val1 + val2;
-}
-function subt(val1, val2) {
-  return val1 - val2;
-}
-function div(val1, val2) {
-  return val1 / val2;
-}
-function mutl(val1, val2) {
-  return val1 * val2;
-}
-function clr() {
-  return (calcElements[5].innerText = 0); //empty dispaly
-}
-function press(value) {
-  if (value == 0) {
-    pressed.push("0");
-  } else if (value == 1) {
-    pressed.push("1");
-  } else if (value == 2) {
-    pressed.push("2");
-  } else if (value == 3) {
-    pressed.push("3");
-  } else if (value == 4) {
-    pressed.push("4");
-  } else if (value == 5) {
-    pressed.push("5");
-  } else if (value == 6) {
-    pressed.push("6");
-  } else if (value == 7) {
-    pressed.push("7");
-  } else if (value == 8) {
-    pressed.push("8");
-  } else if (value == 9) {
-    pressed.push("9");
-  } else if (value == ".") {
-    pressed.push(".");
-  }
-  console.log(pressed);
-  return pressed;
-}
+// let calcElements = [
+// document.querySelectorAll(".Num"),
+// document.querySelectorAll(".operator"),
+// document.querySelector(".dot"),
+// document.querySelector(".clear"),
+//   document.querySelector("#equals"),
+//   document.querySelector("#display"),
+// ];
+// let pressed = []; //array to save 1)1st pressed button 2)operator 3)2nd pressed button 4)equals
+// let operator = []; //to know which operator is pressed
+// let flag = false; // if operator is pressed this will become true
+// function add(val1, val2) {
+//   return val1 + val2;
+// }
+// function subt(val1, val2) {
+//   return val1 - val2;
+// }
+// function div(val1, val2) {
+//   return val1 / val2;
+// }
+// function mutl(val1, val2) {
+//   return val1 * val2;
+// }
+// function clr() {
+//   return (calcElements[5].innerText = 0); //empty dispaly
+// }
+// function press(value) {
+//   if (value == 0) {
+//     pressed.push("0");
+//   } else if (value == 1) {
+//     pressed.push("1");
+//   } else if (value == 2) {
+//     pressed.push("2");
+//   } else if (value == 3) {
+//     pressed.push("3");
+//   } else if (value == 4) {
+//     pressed.push("4");
+//   } else if (value == 5) {
+//     pressed.push("5");
+//   } else if (value == 6) {
+//     pressed.push("6");
+//   } else if (value == 7) {
+//     pressed.push("7");
+//   } else if (value == 8) {
+//     pressed.push("8");
+//   } else if (value == 9) {
+//     pressed.push("9");
+//   } else if (value == ".") {
+//     pressed.push(".");
+//   }
+//   console.log(pressed);
+//   return pressed;
+// }
 
-let sum1 = 0;
-for (var i = 0; i < pressed.length; i++) sum1 += pressed[i];
-// let pressedValue = parseInt(sum1);
-console.log(sum1);
+// let sum1 = 0;
+// for (var i = 0; i < pressed.length; i++) sum1 += pressed[i];
+// // let pressedValue = parseInt(sum1);
+// console.log(sum1);
 // console.log(pressedValue);
 // function operator(value) {
 //   if (value == "*") {
